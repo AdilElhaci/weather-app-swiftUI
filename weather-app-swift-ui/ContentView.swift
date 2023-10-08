942a84bae45a74e8833c938397a3182fddd1d18d
adilelhaci <adelalhajji97@gmail.com>
2023-10-08 12:17:50 +0300

    init commit

---
+++ weather-app-swift-ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather-app-swift-ui
+//
+//  Created by Adil Elhaci on 8.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather-app-swift-ui/weather_app_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_swift_uiApp.swift
+//  weather-app-swift-ui
+//
+//  Created by Adil Elhaci on 8.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
